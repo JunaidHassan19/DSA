0cef772e58ed257cea26b4cc2999d20522548b6b
JunaidHassan19 <thejunni01@gmail.com>
2026-01-10 12:23:38 +0530

    Arrays and Strings (Basic)

--- 01-Basics/1.2-Things to Know in C++/1.2.5.cpp
+++ 01-Basics/1.2-Things to Know in C++/1.2.5.cpp
@@ -14,3 +14,16 @@ int main()
 
   return 0;
 }
+
+/*
+What is an Array?
+An array is a linears data structure in which we store data and perform any operation, we can randomly access data in an array (With the help of its index values).
+
+In other words, an array is a collection of similar types of elements (Homogeneous elements) that have contiguous memory locations i.e One after another.
+
+Arrays store the related information in adjacent memory blocks.
+
+If you need to access one or more than one element, the process of finding or performing operations on that element becomes very fast because your computer very well knows where the value is located in the memory.
+
+
+*/
