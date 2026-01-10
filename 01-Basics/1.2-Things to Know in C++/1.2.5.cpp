cd6861205b3ed01782cbf579df1b618d83f94871
JunaidHassan19 <thejunni01@gmail.com>
2026-01-10 12:21:47 +0530

    Arrays and Strings (Basic)

---
+++ 01-Basics/1.2-Things to Know in C++/1.2.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int arr[5] = {5, 4, 6, 7, 2};
+  // int arr[5];
+  // cout << "Enter the Array elements: ";
+  // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+  cout << arr[3];
+  // cout << arr[0];
+  // cout << arr[1];
+
+  return 0;
+}
