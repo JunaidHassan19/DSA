dd16511e0f691a3cca478d2b4e634bcf974d265b
JunaidHassan19 <thejunni01@gmail.com>
2026-01-11 12:03:01 +0530

    Functions (Pass by Reference and Value)

--- 01-Basics/1.2-Things to Know in C++/1.2.8.cpp
+++ 01-Basics/1.2-Things to Know in C++/1.2.8.cpp
@@ -11,59 +11,85 @@ using namespace std;
   - non-parameterised -> Function that takes no parameters
 */
 
-//********************************
+//*****************************************************
 // EXAMPLE 1: Non-Parameterised Function (No input, No return)
 // This function takes no arguments and prints a fixed message
 
-void printName()
+// void printName()
+// {
+//   cout << "Hey";
+// }
+
+// int main()
+// {
+//   printName(); // Calling the function - Output: Hey
+//   return 0;
+// }
+
+// //****************************************************
+// // EXAMPLE 2: Parameterised Function (With input, No return)
+// // This function accepts a 'name' parameter and uses it in output
+
+// void printName(string name)
+// {
+//   cout << "Hey " + name; // Concatenates "Hey " with the name passed
+// }
+
+// int main()
+// {
+//   string name;
+//   cout << "Enter your name: ";
+//   cin >> name; // Takes user input
+
+//   printName(name); // Passes 'name' as argument to the function
+//   return 0;
+// }
+
+// // ***************************************************
+// // EXAMPLE 3: Function with Multiple Variables
+// // Shows how to use multiple variables and pass any of them to a function
+
+// void printName(string name)
+// {
+//   cout << "Hey " + name << endl;
+// }
+
+// int main()
+// {
+//   string name;
+//   cout << "Enter your name: ";
+//   cin >> name; // First user input stored in 'name'
+
+//   string name2;
+//   cout << "Enter name2: ";
+//   cin >> name2; // Second user input stored in 'name2'
+
+//   printName(name2); // Passing 'name2' to the function instead of 'name'
+//   return 0;
+// }
+
+// ****************************************************
+// EXAMPLE 4: Parameterised Function with Two Parameters
+// This function takes two integer inputs and prints their sum
+// Demonstrates: passing multiple parameters to a function
+
+void sum(int a, int b)
 {
-  cout << "Hey";
+  int c = a + b;
+  cout << "The sum of " << a << " and " << b << " is " << c;
 }
 
 int main()
 {
-  printName(); // Calling the function - Output: Hey
-  return 0;
-}
+  int x;
+  cout << "Enter first number: ";
+  cin >> x; // First number input
 
-//***********************************
-// EXAMPLE 2: Parameterised Function (With input, No return)
-// This function accepts a 'name' parameter and uses it in output
+  int y;
+  cout << "Enter second number: ";
+  cin >> y; // Second number input
 
-void printName(string name)
-{
-  cout << "Hey " + name; // Concatenates "Hey " with the name passed
-}
+  sum(x, y); // Passing both x and y as arguments to sum()
 
-int main()
-{
-  string name;
-  cout << "Enter your name: ";
-  cin >> name; // Takes user input
-
-  printName(name); // Passes 'name' as argument to the function
-  return 0;
-}
-
-// *******************************
-// EXAMPLE 3: Function with Multiple Variables
-// Shows how to use multiple variables and pass any of them to a function
-
-void printName(string name)
-{
-  cout << "Hey " + name << endl;
-}
-
-int main()
-{
-  string name;
-  cout << "Enter your name: ";
-  cin >> name; // First user input stored in 'name'
-
-  string name2;
-  cout << "Enter name2: ";
-  cin >> name2; // Second user input stored in 'name2'
-
-  printName(name2); // Passing 'name2' to the function instead of 'name'
   return 0;
 }
