43ebd6d7d990b779a7e6b3c7aef96149274d7931
JunaidHassan19 <thejunni01@gmail.com>
2026-01-12 11:14:25 +0530

    Rectangular Star Pattern

---
+++ 01-Basics/1.3-Build-up Logical Thinking/1.3.1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+// Pattern Printing Rules:
+// 1. Outer loop: Controls the number of rows (iterates x times)
+// 2. Inner loop: Controls the number of columns per row (iterates y times)
+// 3. Print the character "*" inside the inner loop for each column
+
+/*
+int main()
+{
+  // int x, y;
+  // cout << "Enter the number of Rows: ";
+  // cin >> x;
+  // cout << "Enter the number of Colunm: ";
+  // cin >> y;
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+*/
+
+void print(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+int main()
+{
+  int n;
+  cout << "Enter the number of R&C: ";
+  cin >> n;
+  print(n);
+}
