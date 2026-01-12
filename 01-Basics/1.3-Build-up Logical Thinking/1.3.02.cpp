02cc0e289cd754bb329dc417bf0e3ef09f108320
JunaidHassan19 <thejunni01@gmail.com>
2026-01-12 12:08:50 +0530

    Right-Angled Triangle Pattern

--- 01-Basics/1.3-Build-up Logical Thinking/1.3.02.cpp
+++ 01-Basics/1.3-Build-up Logical Thinking/1.3.02.cpp
@@ -15,6 +15,22 @@ void print(int n)
 
 int main()
 {
+  /*
+  // This section allows running multiple test cases
+  // t = number of test cases to run
+  // For each test case, user enters 'n' (rows & columns)
+  // and the right-angled triangle pattern is printed
+
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    int n;
+    cout << "Enter the number of R&C: ";
+    cin >> n;
+    print(n);
+  }
+  */
   int n;
   cout << "Enter the number of R&C: ";
   cin >> n;
