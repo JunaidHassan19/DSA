37f2557a1702a34678378d0ab312b84bb8714878
JunaidHassan19 <thejunni01@gmail.com>
2026-01-19 11:26:32 +0530

    VECTOR -PRINTING A VECTOR

---
+++ 01-Basics/1.4-STL/1.3.4.cpp
@@ -0,0 +1,30 @@
+/*
+ * ============================================
+ * PRINTING A VECTOR
+ * ============================================
+ *
+ * A vector is a dynamic array. To print all elements,
+ * you can use a range-based for loop.
+ *
+ * Basic Example:
+ *   vector<int> v = {10, 20, 30};
+ *   for (int x : v) cout << x << " ";
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> v = {10, 20, 30, 40};
+
+	cout << "Vector elements: ";
+	for (int x : v)
+	{
+		cout << x << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
